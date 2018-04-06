d19c46edaa97051d718b41d19a0fd03184710879
Unknown <nefayran@gmail.com>
2018-04-06 20:11:30 +0300

    Add listing dirs

--- server.js
+++ server.js
@@ -3,16 +3,18 @@ var express = require('express');
 var app = express();
 var http = require('http');
 var fs = require('fs');
-//app.use(express.bodyParser());
+
+global.appRoot = path.resolve(__dirname);
+
 app.use(express.json());
 app.use(express.urlencoded());
-//app.use(express.multipart());
 
 app.post('/', function(req, res) {
-  console.log(req.body);
-  res.send(200);
-
-  foo();
+    readDir(global.appRoot);
+    var reply = "";
+    //console.log(readDir(global.appRoot));
+    reply = "Dirs and files: " + readDir(global.appRoot);
+    res.send(reply);
 });
 
 app.get('/', function(req, res) {
@@ -23,17 +25,13 @@ app.get('/js/ui.js',function(req,res){
     res.sendFile(path.join(__dirname + '/js/ui.js'));
 });
 
-var foo = function() {
-  var path = '/etc/';
-  fs.readdir(path, function(err, items) {
-    console.log(items);
+var dir = [];
+function readDir(path) {
+  return fs.readdirSync(path, function(err, items) {
+      return items;
+  });
+}
 
-    for (var i=0; i<items.length; i++) {
-        console.log(items[i]);
-    }
-});
-};
 
 var server = http.createServer(app);
-
 server.listen(8080);
